f100b03642bbc7516fbca2fbbbcaf3605bb6295d
Tony Baker <this.tonybaker@gmail.com>
2019-07-24 18:43:36 +0000

    Automatically backed up by Learn

---
+++ app/controllers/coupon_controller.rb
@@ -0,0 +1,16 @@
+class CouponsController < ApplicationController
+  def index
+    @coupon = Coupons.all
+  end
+
+  def show
+    @coupon = Coupon.find_by(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.new
+    
+end
